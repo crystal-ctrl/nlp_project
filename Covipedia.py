5e6a1d40840c83317334aa0030b6de8ae5def535
Crystal <77818135+crystal-ctrl@users.noreply.github.com>
2021-05-26 21:24:51 -0400

    update app

--- Covipedia.py
+++ Covipedia.py
@@ -11,17 +11,13 @@ from PIL import Image
 ################
 
 image = Image.open('Covipedia.png')
-col1, col2 = st.beta_columns([3,5])
+col1, col2 = st.beta_columns([3,8])
 with col1:
-    st.image(image, width=200)
+    st.image(image, width=150)
 with col2:
-    st.write("""
-# COVIPEDIA
+    st.title("COVIPEDIA")
+    st.markdown("Navigate COVID-19 scientific literature with **Topics**!")
 
-Navigate COVID-19 scientific literature with **Topics**!
-
-"""
-)
 st.markdown("""---""")
 ################
 ##   Input    ##
@@ -30,8 +26,8 @@ st.markdown("""---""")
 st.subheader('COVID-19 Publications Related to: ')
 
 option = st.selectbox(
-    "Choose the topics to explore:",
-    ("Education and Training", "Enviromental Impact", "Inhibition of COVID-19",
+    "Choose the topic to explore:",
+    ("Education and Training", "Enviromental Impact", "Inhibition on COVID-19",
     "Complications of COVID-19", "Diagnositc Imaging and Surveillance", "Mental Health",
     "Information sharing and Inter-sectoral Collaboration", "ACE2 Receptor",
     "Ethical and Social Considerations", "Material Science", "Diagnositc Testings",
@@ -39,7 +35,7 @@ option = st.selectbox(
     "Treatments and Therapeutics", "Personal Protective Equipment", "Transmission and Mortality Rate",
     "Technology Application", "Cancer", "Social Behavior Research", "Policy and Procedure",
     "Alternative Treatments and Therapies", "Medical Care", "Non-Pharmaceutical Interventions",
-    "Social Network", "Long-Term Effects of COVID-19", "Machine Learning Application",
+    "Social Network", "Long-Term Effects of COVID-19", "Data Science Tools and Algorithms",
     "Critical Care")
 )
 st.markdown("""---""")
@@ -47,14 +43,15 @@ st.markdown("""---""")
 ################
 ##   Output   ##
 ################
-st.write("**Here are the top 20 articles related to: **", option)
+st.write("**Here are the articles related to: **", option, "(sorted by relevance)")
+
 
 # import dataset
-data = pd.read_csv("app_ready.csv")
+data = pd.read_csv("app_topics.csv")
 
 #Convert option string to topic_num
 def option2num(option):
-    option_map = {"Education and Training":0, "Enviromental Impact":1, "Inhibition of COVID-19":2,
+    option_map = {"Education and Training":0, "Enviromental Impact":1, "Inhibition on COVID-19":2,
     "Complications of COVID-19":3, "Diagnositc Imaging and Surveillance":4, "Mental Health":5,
     "Information sharing and Inter-sectoral Collaboration":6, "ACE2 Receptor":7,
     "Ethical and Social Considerations":8, "Material Science":9, "Diagnositc Testings":10,
@@ -62,14 +59,14 @@ def option2num(option):
     "Treatments and Therapeutics":14, "Personal Protective Equipment":15, "Transmission and Mortality Rate":16,
     "Technology Application":17, "Cancer":18, "Social Behavior Research":19, "Policy and Procedure":20,
     "Alternative Treatments and Therapies":21, "Medical Care":22, "Non-Pharmaceutical Interventions":23,
-    "Social Network":24, "Long-Term Effects of COVID-19":25, "Machine Learning Application":26,
+    "Social Network":24, "Long-Term Effects of COVID-19":25, "Data Science Tools and Algorithms":26,
     "Critical Care":27}
     return option_map[option]
 
 # function to get top 20 articles
 def topic_relative_paper(topic_num):
     df = data[data['Topic_Num'] == topic_num].reset_index(drop=True)
-    for i in range(20):
+    for i in range(len(df)):
         link = f'{df.Title[i]} \n({df.url[i]})'
         st.write(link, unsafe_allow_html=True)
 
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sh setup.sh && streamlit run myapp.py
+web: sh setup.sh && streamlit run Covipeida.py
